196ed4321023962e7e43410ea818602fca2ae5ce
hmr53 <160482647+hmr53@users.noreply.github.com>
2024-02-22 21:15:18 +0530

    Initial Commit

---
+++ hello-world-restful-web-services/src/main/java/com/hasith/helloworldrestfulwebservices/HelloWorldRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.hasith.helloworldrestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldRestfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ hello-world-restful-web-services/src/test/java/com/hasith/helloworldrestfulwebservices/HelloWorldRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasith.helloworldrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
